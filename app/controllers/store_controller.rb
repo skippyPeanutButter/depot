eaa63b7e49fb2520914a4d95616799a4666b1f7f
lortiz <lortiz1145@gmail.com>
2015-01-03 22:20:14 -0500

    added table styling to cart show page

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,14 @@
+class StoreController < ApplicationController
+  def index
+    @products = Product.order(:title)
+    @time = Time.now
+    @counter = increment_count
+  end
+
+
+  private
+    def increment_count
+      session[:counter] ||= 0
+      session[:counter] += 1
+    end
+end
