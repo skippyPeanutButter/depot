762d831f0a89ca408ef6036d845c008cf7de8d6e
lortiz <lortiz1145@gmail.com>
2015-01-07 20:41:18 -0500

    Moved payment types to DB

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,10 @@
 class Order < ActiveRecord::Base
+  belongs_to :pay_type
   has_many :line_items, dependent: :destroy
-
-  PAYMENT_TYPES = ["Check", "Credit Card", "Purchase order"]
 
   validates :name, :email, :address, presence: true
 
-  validates :pay_type, inclusion: PAYMENT_TYPES
+  validates :pay_type, inclusion: PAYMENT_TYPES = ["Check", "Credit Card", "Purchase order"]
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -13,4 +12,5 @@ class Order < ActiveRecord::Base
       line_items << item
     end
   end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,16 @@ Depot::Application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               "lortiz1145@gmail.com",
+      authentication:       "plain",
+      user_name:            "Luis",
+      password:             "secret",
+      enable_starttls_auto: true
+  }
 end
